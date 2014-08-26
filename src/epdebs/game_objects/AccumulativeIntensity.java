7d4424ad09a96d3255f32ab631198bf3ee628cf6
Hamid Elmi <Hamid.elmi@gmail.com>
2014-08-26 17:16:44 +0200

    Q#2

---
+++ src/epdebs/game_objects/AccumulativeIntensity.java
@@ -0,0 +1,27 @@
+package epdebs.game_objects;
+
+public class AccumulativeIntensity {
+	public long ts;
+	public long lastIntTs;
+	public int intensity;
+	public long v;
+	public boolean active;
+
+	public static AccumulativeIntensity Instantiate(String name, long ts,
+			long abs_v, int instanceIntensity) {
+		Game game = Game.Singleton();
+		AccumulativeIntensity current = game.getIntensity(name);
+		if (current == null)
+			current = new AccumulativeIntensity();
+
+		if (current.active) {
+
+			current.active = current.intensity == instanceIntensity;
+			current.ts = ts;
+			current.v = abs_v;
+			current.intensity = instanceIntensity;
+		}
+
+		return current;
+	}
+}
---
+++ src/esper/Query2Main.java
@@ -0,0 +1,57 @@
+package esper;
+
+import com.espertech.esper.client.Configuration;
+import com.espertech.esper.client.EPAdministrator;
+import com.espertech.esper.client.EPRuntime;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+import epdebs.game_objects.Game;
+import epdebs.parser.DataFileParser;
+import epdebs.parser.Event;
+import epdebs.parser.Settings;
+import epdebs.queries.AccumulativeIntensityStatement;
+import epdebs.queries.IntensityListener;
+import epdebs.queries.IntensityStatement;
+
+public class Query2Main {
+
+	public static class CEPListener implements UpdateListener {
+
+		public void update(EventBean[] newData, EventBean[] oldData) {
+			System.out.println("Event received: " + newData[0].getUnderlying());
+		}
+	}
+
+	public static void main(String[] args) {
+		Settings.dataFilePath = System.getenv("dataFilePath");
+		Settings.metadataFilePath = System.getenv("metadataFilePath");
+
+		// The Configuration is meant only as an initialization-time object.
+		Configuration cepConfig = new Configuration();
+		cepConfig.addEventType("Event", Event.class.getName());
+		EPServiceProvider cep = EPServiceProviderManager.getProvider(
+				"myCEPEngine", cepConfig);
+		EPRuntime cepRT = cep.getEPRuntime();
+		EPAdministrator cepAdm = cep.getEPAdministrator();
+		IntensityStatement intensityStatement = new IntensityStatement(cepAdm,
+				null);
+
+		AccumulativeIntensityStatement accumulativeIntensityStatement = new AccumulativeIntensityStatement(
+				cepAdm, new IntensityListener());
+
+		DataFileParser parser = new DataFileParser();
+		Game game = Game.Singleton();
+
+		Event curPosEvent = parser.createNewEvent();
+
+		int c = 0;
+		while (curPosEvent != null && c++ < 10) {
+			cep.getEPRuntime().sendEvent(curPosEvent);
+
+			curPosEvent = parser.createNewEvent();
+		}
+	}
+}
