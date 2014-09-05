b38ff82cb20ef7409b192b2ed97ab984bf4deaad
Arun Tewatia <contact.arun.tewatia@gmail.com>
2014-09-05 09:47:59 +0200

    Game Update

--- src/eplab/anfragen/Game.java
+++ src/eplab/anfragen/Game.java
@@ -5,15 +5,14 @@ package eplab.anfragen;
  * Also it holds all the utility functions needed in all queries 
  * 
  */
-import epdebs.game_objects.Intensitiy;
 import eplab.bodenobjekte.*;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -22,7 +21,7 @@ import java.util.List;
 
 public class Game {
 	private static Game instance;
-	protected Team teamA, teamB;
+	protected static Team teamA, teamB;
 	protected Referee referee;
 	protected Ball ball;
 	private HashMap<String, AccumulativeIntensity> playersAccumulativeIntensity;
@@ -31,7 +30,12 @@ public class Game {
 	protected static int leftField = 0;
 	protected static int topField = 33960;
 	protected static int rightField = 52483;
-
+	protected static long tsStartFirstHalf = Long
+			.parseLong("10753295594424116");
+	protected static long tsEndFirstHalf = Long.parseLong("12557295594424116");
+	protected static long tsStartSecondHalf = Long
+			.parseLong("13086639146403495");
+	protected static long tsEndSecondHalf = Long.parseLong("14879639146403495");
 	protected static ArrayList<Double> beginTS = new ArrayList<Double>();
 	protected static ArrayList<Double> endTS = new ArrayList<Double>();
 	protected static int initTsArray = 0;
@@ -138,6 +142,48 @@ public class Game {
 		return null;
 	}
 
+	public static String getPlayerName(String sensorId) {
+		int x = Integer.parseInt(sensorId);
+		if(x == 105 || x == 106 )
+			{return "Refree";}
+		else if (x == 4 || x == 8 || x == 10 || x == 12)
+		{ return "Ball"; }
+		
+		for (int i = 0; i < teamA.players.size(); i++){
+			if (teamA.players.get(i).hasSensorId(x)) {
+				return teamA.players.get(i).name;
+			}else
+				continue;
+		
+		}
+		for (int i = 0; i < teamB.players.size(); i++){
+			if (teamB.players.get(i).hasSensorId(x)) {
+				return teamB.players.get(i).name;
+				}else
+					continue;
+		}
+		
+		return null;
+	}
+
+	public static String getTeam(String playerName) {
+	
+		for (int i = 0; i < teamA.players.size(); i++){
+			if (teamA.players.get(i).getName().equals(playerName)) {
+				return "teamA";
+				}else
+					continue;
+		}
+		for (int i = 0; i < teamB.players.size(); i++){
+			if (teamB.players.get(i).getName().equals(playerName)) {
+				return "teamB";
+				}else
+					continue;
+		}
+		
+		return null;
+	}
+
 	public static double calculateSpeed(double v) {
 		return v * 3600D * Math.pow(10, -9);
 	}
@@ -159,20 +205,20 @@ public class Game {
 			return Intensitiy.Sprint;
 	}
 
-	public static int StatusInField(long x, long y, long z) {
+	public static String StatusInField(long x, long y, long z) {
 		if ((x < leftField) || (x > rightField) || (y > topField)
 				|| (y < bottomField)) {
-			return -1;
+			return "-1";
 		}
 		if ((x > 22578.5D) && (x < 29898.5D) && (y >= 33941.0D)
 				&& (z < 2440.0D)) {
-			return 1;
+			return "1";
 		}
 		if ((x > 22560.0D) && (x < 29880.0D) && (y <= -33968.0D)
 				&& (z < 2440.0D)) {
-			return 2;
+			return "2";
 		}
-		return 0;
+		return "0";
 	}
 
 	public static long GetEuclideanDistance(int x1, int y1, int z1, int x2,
@@ -185,26 +231,27 @@ public class Game {
 		return (long) d;
 	}
 
-	public static boolean isValidInterval(Double ts) throws IOException {
+	public static boolean isValidInterval(double ts) throws IOException {
 
 		int y;
+		long gameHalf = 0;
+		;
 		if (initTsArray == 0) {
 			BufferedReader br = new BufferedReader(new FileReader(
 					Settings.matchIntervalFilePath));
 			String[] splitLine = null;
 			String splitString = null;
-			int gameHalf = 0;
-			int event_id = 0;
+
 			while ((splitString = br.readLine()) != null) {
 				splitLine = splitString.toString().split(";");
 				if (splitLine[0].startsWith("20")) {
-					gameHalf = 1;
+					gameHalf = tsStartFirstHalf;
 				} else
-					gameHalf = 2;
-				if (splitLine[2].split(" ")[2].equals("Begin")) {
-					beginTS.add(convertStringtoMilliSec(splitLine[2]));
+					gameHalf = tsStartSecondHalf;
+				if (splitLine[1].split(" ")[2].equals("Begin")) {
+					beginTS.add(convertStringtoMilliSec(splitLine[2], gameHalf));
 				} else {
-					endTS.add(convertStringtoMilliSec(splitLine[2]));
+					endTS.add(convertStringtoMilliSec(splitLine[2], gameHalf));
 				}
 			}
 			br.close();
@@ -212,34 +259,59 @@ public class Game {
 		}
 
 		for (Double s : beginTS) {
-			if (ts.compareTo(s) <= 0) {
-				y = beginTS.indexOf(s-1);
-				if (ts.compareTo(endTS.get(y)) <= 0) {
+			Double db = new Double(ts);
+			db = db * Math.pow(10.0D, -12.0D);
+			if (db.compareTo(s) <= 0) {
+				y = beginTS.indexOf(s) - 1;
+				if (db.compareTo(endTS.get(y)) <= 0) {
 					return true;
-				} 
+				} else
+					return false;
 			}
-			
+
 		}
 		return false;
 
 	}
 
-	public static double convertStringtoMilliSec(String currTime) {
+	public static double convertStringtoMilliSec(String currTime, long gameHalf) {
 		String sTimeStampFormat = "hh:mm:ss.SSS";
 		SimpleDateFormat intervalTimeFormat = new SimpleDateFormat(
 				sTimeStampFormat);
 		Calendar baseCal = new GregorianCalendar();
+
 		try {
 			baseCal.setTime(intervalTimeFormat.parse("00:00:00.000"));
 			Date parsedDate = intervalTimeFormat.parse(currTime);
 			Calendar tmpCal = new GregorianCalendar();
 			tmpCal.setTime(parsedDate);
 
-			return tmpCal.getTimeInMillis() - baseCal.getTimeInMillis();
+			// double x = tmpCal.getTimeInMillis();
+			// double y = baseCal.getTimeInMillis();
+
+			return (parsedDate.getTime() - baseCal.getTimeInMillis())
+					* Math.pow(10D, -3.0D) + gameHalf * Math.pow(10D, -12D);
+
 		} catch (Exception e) {
-			System.out.println("error converting ts to pico seconds");
-			return 1800000L;
+		//	System.out.println("error converting ts to pico seconds");
+			return 0L;
 		}
 
 	}
+
+	public static boolean isBall(String sensorId) {
+		int[] ballSid = new int[] { 4, 8, 10, 12 };
+		for( int x = 0; x < ballSid.length ; x++)
+		{
+			if(ballSid[x] == Integer.parseInt(sensorId))
+				return true;
+		}
+//		if (Arrays.asList(ballSid).contains(Integer.parseInt(sensorId)))
+//			return true;
+//		else
+//			return false;
+
+		return false;
+	}
+
 }
