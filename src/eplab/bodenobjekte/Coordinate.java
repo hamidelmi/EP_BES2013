9197e64624fecd74f671e3211e521b52ce025e64
Arun Tewatia <contact.arun.tewatia@gmail.com>
2014-09-05 10:15:00 +0200

    Q2

---
+++ src/eplab/bodenobjekte/Coordinate.java
@@ -0,0 +1,25 @@
+package eplab.bodenobjekte;
+
+public class Coordinate {
+//	protected long x, y, z;
+	public long x, y, z;
+
+	public Coordinate(long x, long y, long z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public long getX() {
+		return this.x;
+	}
+
+	public long getY() {
+		return this.y;
+	}
+
+	public long getZ() {
+		return this.z;
+	}
+
+}
